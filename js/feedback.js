ca9bdc6c809e3582e9ac9772bd9c7a4617e3dd51
Louis Stanard <louis.stanard@gmail.com>
2013-06-02 22:29:03 -0500

    Partial commit of recent updates

--- js/feedback.js
+++ js/feedback.js
@@ -1,34 +1,62 @@
-$(document).ready(function(){
-
+var init = function() {
+	
+	// Create global variables
+	var i = 0;
 	// Object that contains all individual comments
 	var comments = {};
-	
-	// Object for each individual comment
-	var comment = {};
-	
-	    comment.contents = {
-	    	comment_author: "",
-	    	comment_copy: ""
-	    }
-	
-	// Retrieve all comments
-	var all_comments = $(".comment"),
-	    i = 0;
-	
-	all_comments.each(function(){
-		
-		var comment_copy = $(this).find('.comment-copy');
-		var comment_author = $(this).find('.comment-author');
-		
-		comment.contents[comment_copy] = comment_copy;
-		comment.contents[comment_author] = comment_author;
-		
-		comments[i] = comment.contents;
-		
+
+	// Utility functions 
+	var create_comment_obj = function(comment_object) {
+		var comment_copy = comment_object.find('.comment-copy').text();
+		var comment_author = comment_object.find('.comment-author').text();
+		var new_comment = new comment();
+		new_comment.contents.comment_author = comment_author;
+		new_comment.contents.comment_copy = comment_copy;
+		comments[i] = new_comment;
 		i++;
-		
-	});
+	}
 	
+	// Setup object for each individual comment
+	var comment = function () {
+		this.contents = {
+			comment_author: "",
+			comment_copy: ""
+		}
+	}
+		
+	// Loop through all comments, parse the parts, create a new comment object and add to the comments object 
+	var all_comments = $(".comment");
+	all_comments.each(function(){
+		create_comment_obj($(this));
+	});
+
+	// Event handlers
+	$('#add-comment-btn').click(function(event){
+		var new_comment_author = $('#add-comment-author').val();
+		var new_comment_copy = $('#add-comment-copy').val();
+		
+		event.preventDefault();
+	});
+
+	$('.comment').delegate(".comment-delete", "click",  function(){
+		event.preventDefault();
+	});
+
 	console.log(comments);
+
+}
+
+$(document).ready(function(){
+
+	/* 
+
+		REFERENCE: 
+		----------
+		https://github.com/rwldrn/idiomatic.js/#comments 
+		http://stackoverflow.com/questions/3913103/javascript-object-literal-pattern-with-multiple-instances 
+
+	*/ 
+
+	init();
 	
 });
